01469175f2ec7c1dd0745126a3185d5017351d41
Ricardo Oliveira <ricardo.oliveira1307@gmail.com>
2022-10-13 11:57:13 -0300

    Criação do Componente PostCard da Page Blog

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,11 +1,21 @@
+import { PostCard } from './components/PostCard'
 import { Profile } from './components/Profile'
 import { SearchInput } from './components/SearchInput'
+import { PostsListContainer } from './styles'
 
 export function Blog() {
   return (
     <>
       <Profile />
       <SearchInput />
+      <PostsListContainer>
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+      </PostsListContainer>
     </>
   )
 }
