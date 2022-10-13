f8385c109428e97da981148c83a3cf6a05b811b8
Ricardo Oliveira <ricardo.oliveira1307@gmail.com>
2022-10-13 20:13:23 -0300

    Adição da funcionalidade de pesquisa dos posts

--- src/pages/Blog/components/SearchInput/index.tsx
+++ src/pages/Blog/components/SearchInput/index.tsx
@@ -1,14 +1,42 @@
+import { useForm } from 'react-hook-form'
+import * as zod from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { SearchInputContainer } from './styles'
 
-export function SearchInput() {
+const searchFormSchema = zod.object({
+  query: zod.string(),
+})
+
+type SearchFormInput = zod.infer<typeof searchFormSchema>
+
+interface SearchInputProps {
+  postsLength: number
+  getPosts: (query?: string) => Promise<void>
+}
+
+export function SearchInput({ postsLength, getPosts }: SearchInputProps) {
+  const { register, handleSubmit } = useForm<SearchFormInput>({
+    resolver: zodResolver(searchFormSchema),
+  })
+
+  async function handleSearchPosts(data: SearchFormInput) {
+    await getPosts(data.query)
+  }
+
   return (
-    <SearchInputContainer>
+    <SearchInputContainer onSubmit={handleSubmit(handleSearchPosts)}>
       <header>
         <h3>Publicações</h3>
-        <span>6 publicações</span>
+        <span>
+          {postsLength >= 2
+            ? `${postsLength} publicações`
+            : postsLength === 1
+            ? `${postsLength} publicação`
+            : 'Nenhuma publicação'}
+        </span>
       </header>
 
-      <input type="text" placeholder="Buscar conteúdo" />
+      <input type="text" placeholder="Buscar conteúdo" {...register('query')} />
     </SearchInputContainer>
   )
 }
--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -46,7 +46,7 @@ export function Blog() {
   return (
     <>
       <Profile />
-      <SearchInput />
+      <SearchInput postsLength={posts.length} getPosts={getPosts} />
       <PostsListContainer>
         {isLoading ? (
           <p>Carregando...</p>
